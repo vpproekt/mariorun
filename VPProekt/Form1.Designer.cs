d9baca696dffd8784bb62128e689b5da2b869069
vpproekt <70815921+vpproekt@users.noreply.github.com>
2020-09-05 19:57:33 +0200

    Add files via upload

---
+++ VPProekt/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace VPProekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.mario = new System.Windows.Forms.PictureBox();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.floor = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 16;
+            this.timer1.Tick += new System.EventHandler(this.gameEvent);
+            // 
+            // mario
+            // 
+            this.mario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.mario.Image = global::VPProekt.Properties.Resources.mario_run1;
+            this.mario.Location = new System.Drawing.Point(50, 360);
+            this.mario.Name = "mario";
+            this.mario.Size = new System.Drawing.Size(32, 56);
+            this.mario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mario.TabIndex = 3;
+            this.mario.TabStop = false;
+            // 
+            // enemy1
+            // 
+            this.enemy1.Image = ((System.Drawing.Image)(resources.GetObject("enemy1.Image")));
+            this.enemy1.Location = new System.Drawing.Point(236, 384);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(32, 32);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 4;
+            this.enemy1.TabStop = false;
+            this.enemy1.Tag = "enemy";
+            // 
+            // enemy2
+            // 
+            this.enemy2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.enemy2.Image = ((System.Drawing.Image)(resources.GetObject("enemy2.Image")));
+            this.enemy2.Location = new System.Drawing.Point(486, 360);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(32, 56);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 5;
+            this.enemy2.TabStop = false;
+            this.enemy2.Tag = "enemy";
+            // 
+            // floor
+            // 
+            this.floor.Image = ((System.Drawing.Image)(resources.GetObject("floor.Image")));
+            this.floor.Location = new System.Drawing.Point(-2, 416);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(957, 49);
+            this.floor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.floor.TabIndex = 6;
+            this.floor.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.mario);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.enemy2);
+            this.Controls.Add(this.enemy1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mario Run";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.mario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox mario;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy2;
+        private System.Windows.Forms.PictureBox floor;
+    }
+}
+
---
+++ VPProekt/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPProekt
+{
+    public partial class Form1 : Form
+    {
+        int jumpSp, jumpVel, score, enemySp;
+        bool jump;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            resetGame();
+        }
+
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !jump && timer1.Enabled)
+            {
+                jump = true;
+                mario.Image = Properties.Resources.mario_jump;
+            }
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                resetGame();
+            }
+
+            if (jump && timer1.Enabled)
+            {
+                mario.Image = Properties.Resources.mario_run1;
+                jump = false;
+            }
+        }
+        private void gameEvent(object sender, EventArgs e)
+        {
+            mario.Top += jumpSp;
+            label1.Text = "Points: " + score;
+
+            if (jump && jumpVel < 0)
+            {
+                jump = false;
+                mario.Image = Properties.Resources.mario_run1;
+            }
+
+            if (jump)
+            {
+                jumpSp = -12;
+                jumpVel -= 1;
+            }
+            else
+            {
+                jumpSp = 12;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "enemy")
+                {
+                    x.Left -= enemySp;
+
+                    if (x.Left + x.Width < -120)
+                    {
+                        x.Left = this.ClientSize.Width + rnd.Next(200, 800);
+                        score++;
+                    }
+
+                    if (mario.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        timer1.Stop();
+                        mario.Size = new System.Drawing.Size(32, 32);
+                        mario.Image = Properties.Resources.mario_oops;
+                        label1.Text = "Press R to Restart";
+                    }
+
+                }
+            }
+
+            if (mario.Top >= 300 && !jump)
+            {
+                jumpVel = 12;
+                mario.Top = floor.Top - mario.Height;
+                jumpSp = 0;
+            }
+
+            if (score >= 15)
+            {
+                enemySp = 15;
+                if (score >= 30)
+                {
+                    enemySp = 20;
+                }
+            }
+
+            if (score == 50)
+            {
+                timer1.Stop();
+                label1.Text = "Congratulations!";
+            }
+        }
+
+        private void resetGame()
+        {
+            jumpVel = 12;
+            jumpSp = 0;
+            score = 0;
+            enemySp = 10;
+            jump = false;
+            label1.Text = "Points: " + score;
+
+            mario.Top = floor.Top - mario.Height;
+            mario.Size = new System.Drawing.Size(32, 56);
+            mario.Image = Properties.Resources.mario_run1;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "enemy")
+                {
+                    int position = rnd.Next(600, 1000);
+                    x.Left = 640 + (x.Left + position + x.Width * 3);
+                }
+            }
+
+            timer1.Start();
+        }
+    }
+}
